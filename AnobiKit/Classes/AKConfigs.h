72120a3f7dfcd60825b045927b5721d38a8f0cd9
Stanislav Pletnev <st.pletnev@gmail.com>
2017-08-04 17:09:47 +0700

    v0.0.9

--- AnobiKit/Classes/AKConfigs.h
+++ AnobiKit/Classes/AKConfigs.h
@@ -15,7 +15,7 @@
 + (NSString *)appGroupIdentifier;
 + (NSURL *)dataContainerURL;
 + (NSURL *)dataFileURLWithName:(NSString *)fn;
-+ (NSURL *)dataFileURLWithName:(NSString *)fn version:(NSInteger)version;
++ (NSURL *)dataFileURLWithName:(NSString *)fn version:(NSUInteger)version;
 
 + (instancetype)shared;
 + (instancetype)new NS_UNAVAILABLE;
