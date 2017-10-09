04f65ddcd4a59d29294dba526a2027674567e62d
Stanislav Pletnev <anobisoft@gmail.com>
2017-10-09 15:21:03 +0700

    AKObject+Mapping property setter not found exception

--- AnobiKit/Classes/AKMapping/AKObject+Mapping.m
+++ AnobiKit/Classes/AKMapping/AKObject+Mapping.m
@@ -46,64 +46,67 @@
             } else {  //automap
                 if ([obj isKindOfClass:[NSDictionary class]]) {
                     objc_property_t property = class_getProperty(object_getClass(self), [propertyKey UTF8String]);
-                    char *type = property_copyAttributeValue(property, "T");
-                    unsigned long len = strlen(type);
-                    Class propertyClass;
-                    
-                    switch (type[0]) {
-                        case '@': {
-                            if (len > 3) {
-                                unsigned long classstrlen;
-                                for (classstrlen = 0; (classstrlen < len && type[classstrlen] != '<') ; classstrlen++) ;
-                                classstrlen -= classstrlen == len ? 3 : 2;
-                                if (classstrlen > 0) {
-                                    char objtypestr[classstrlen+1];
-                                    memcpy(objtypestr, type+2, classstrlen);
-                                    objtypestr[classstrlen] = '\0';
-                                    NSString *className = [NSString stringWithUTF8String:objtypestr];
-                                    propertyClass = NSClassFromString(className);
-                                    NSLog(@"[DEBUG] /AUTOMAP/ @property %@ %@ / Class %@ founded", className, propertyKey, propertyClass ?: @"not");
-
+                    if (property) {
+                        char *type = property_copyAttributeValue(property, "T");
+                        unsigned long len = strlen(type);
+                        Class propertyClass;
+                        
+                        switch (type[0]) {
+                            case '@': {
+                                if (len > 3) {
+                                    unsigned long classstrlen;
+                                    for (classstrlen = 0; (classstrlen < len && type[classstrlen] != '<') ; classstrlen++) ;
+                                    classstrlen -= classstrlen == len ? 3 : 2;
+                                    if (classstrlen > 0) {
+                                        char objtypestr[classstrlen+1];
+                                        memcpy(objtypestr, type+2, classstrlen);
+                                        objtypestr[classstrlen] = '\0';
+                                        NSString *className = [NSString stringWithUTF8String:objtypestr];
+                                        propertyClass = NSClassFromString(className);
+                                        //                                    NSLog(@"[DEBUG] /AUTOMAP/ @property %@ %@ / Class %@ founded", className, propertyKey, propertyClass ?: @"not");
+                                        
+                                    }/* else {
+                                      unsigned long protocollen = len-classstrlen-2-3;
+                                      char objtypestr[protocollen+1];
+                                      memcpy(objtypestr, type+2+classstrlen+1, protocollen);
+                                      objtypestr[protocollen] = '\0';
+                                      NSString *protocolStr = [NSString stringWithUTF8String:objtypestr];
+                                      NSArray<NSString *> *protocols = [protocolStr componentsSeparatedByString:@"><"];
+                                      NSLog(@"[DEBUG] @property id<%@> %@", protocolStr, obj);
+                                      for (NSString *protocolName in protocols) {
+                                      Protocol *protocol = NSProtocolFromString(protocolName);
+                                      unsigned int ppropcount;
+                                      free(protocol_copyPropertyList(protocol, &ppropcount));
+                                      if (ppropcount > 0) {
+                                      NSLog(@"@protocol <%@> property count %u", protocolName, ppropcount);
+                                      }
+                                      }
+                                      } //*/
                                 }/* else {
-                                    unsigned long protocollen = len-classstrlen-2-3;
-                                    char objtypestr[protocollen+1];
-                                    memcpy(objtypestr, type+2+classstrlen+1, protocollen);
-                                    objtypestr[protocollen] = '\0';
-                                    NSString *protocolStr = [NSString stringWithUTF8String:objtypestr];
-                                    NSArray<NSString *> *protocols = [protocolStr componentsSeparatedByString:@"><"];
-                                    NSLog(@"[DEBUG] @property id<%@> %@", protocolStr, obj);
-                                    for (NSString *protocolName in protocols) {
-                                        Protocol *protocol = NSProtocolFromString(protocolName);
-                                        unsigned int ppropcount;
-                                        free(protocol_copyPropertyList(protocol, &ppropcount));
-                                        if (ppropcount > 0) {
-                                            NSLog(@"@protocol <%@> property count %u", protocolName, ppropcount);
-                                        }
-                                    }
-                                } //*/
-                            } else {
-                                NSLog(@"[DEBUG] /AUTOMAP/ @property id %@", propertyKey);
-                            }
-                        } break;
-                        case '*':
-                        case '^':
-                        case '{': {
-                            NSLog(@"[ERROR] /AUTOMAP/ @property %s %@ / class '%@' is not KVC-compliant for the key '%@'", type, propertyKey, self.class, propertyKey);
-                        } break;
-                        default:
-                            NSLog(@"[DEBUG] /AUTOMAP/ @property %s %@", type, propertyKey);
-                            break;
-                    }
-                    
-                    if (propertyClass && [propertyClass isSubclassOfClass:[AKObject class]]) {
-                        id newPropertyInstance = [propertyClass instatiateWithExternalRepresentation:obj objectMap:propertyMap.objectMap];
-                        [self setValue:newPropertyInstance forKey:propertyKey];
-                    } else {
-                        @try {
-                            [self setValue:obj forKey:propertyKey]; // try to set representation object to property as is (NSDictionary)
-                        } @catch (NSException *exception) {
-                            NSLog(@"[ERROR] exception: %@", exception);
+                                  NSLog(@"[DEBUG] /AUTOMAP/ @property id %@", propertyKey);
+                                  } //*/
+                            } break;
+                            case '*':
+                            case '^':
+                            case '{': {
+                                NSLog(@"[ERROR] /AUTOMAP/ @property %s %@ / class '%@' is not KVC-compliant for the key '%@'", type, propertyKey, self.class, propertyKey);
+                            } break;
+                                /*                        default:
+                                 NSLog(@"[DEBUG] /AUTOMAP/ @property %s %@", type, propertyKey);
+                                 break; //*/
                         }
+                        if (propertyClass && [propertyClass isSubclassOfClass:[AKObject class]]) {
+                            id newPropertyInstance = [propertyClass instatiateWithExternalRepresentation:obj objectMap:propertyMap.objectMap];
+                            [self setValue:newPropertyInstance forKey:propertyKey];
+                        } else {
+                            @try {
+                                [self setValue:obj forKey:propertyKey]; // try to set representation object to property as is (NSDictionary)
+                            } @catch (NSException *exception) {
+                                NSLog(@"[ERROR] exception: %@", exception);
+                            }
+                        }
+                    } else {
+                        NSLog(@"[WARNING] Representation keyed '%@' skipped: setter not found.", propertyKey);
                     }
                 } else {
                     @try {
