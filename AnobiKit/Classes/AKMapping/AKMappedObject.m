c8f9f50401dab835302b77484120cf06975f18b5
Stanislav Pletnev <anobisoft@gmail.com>
2018-03-28 16:57:48 +0700

    AKMappedObject -initWithExternalRepresentation improve

--- AnobiKit/Classes/AKMapping/AKMappedObject.m
+++ AnobiKit/Classes/AKMapping/AKMappedObject.m
@@ -6,7 +6,15 @@
 //  Copyright © 2017 Anobisoft. All rights reserved.
 //
 
+@protocol AKMutable
+
+- (void)addObject:(id)object;
+
+@end
+
+
 #import "AKMappedObject.h"
+#import "AKUUID.h"
 @import ObjectiveC.runtime;
 
 @implementation AKMappedObject
@@ -26,45 +34,90 @@
 
 - (instancetype)initWithExternalRepresentation:(NSDictionary *)representation objectMap:(AKObjectMap *)objectMap {
     if (self = [super init]) {
+        if ([representation isKindOfClass:NSNull.class]) {
+            return nil;
+        }
         [representation enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
-            if (!obj) return ;
-            if ([obj isKindOfClass:[NSNull class]]) return ;
             NSString *propertyKey = key;
             AKPropertyMap *propertyMap = objectMap[key];
             if (propertyMap) propertyKey = propertyMap.propertyKey ?: key;
-            Class objectClass = propertyMap.objectClass;
-            if (objectClass && [objectClass conformsToProtocol:@protocol(AKObjectMapping)]) {
-                if ([obj isKindOfClass:[NSArray class]]) {
-                    NSMutableArray *mutableArray = [NSMutableArray new];
-                    for (NSDictionary *objRepresentation in obj) {
-                        id newPropertyInstance = [propertyMap.objectClass instatiateWithExternalRepresentation:objRepresentation objectMap:propertyMap.objectMap];
-                        [mutableArray addObject:newPropertyInstance];
+            objc_property_t property = class_getProperty(object_getClass(self), [propertyKey UTF8String]);
+            if (!property) {
+                NSLog(@"[WARNING] Representation keyed '%@' skipped: setter not found.", propertyKey);
+                return ;
+            }
+            Class propertyClass = property_getClass(property);
+            Class mapClass = propertyMap.objectClass;
+            if (mapClass) {
+                if ([mapClass conformsToProtocol:@protocol(AKObjectMapping)]) {
+                    if ([obj conformsToProtocol:@protocol(NSFastEnumeration)]) {
+                        if (![propertyClass isSubclassOfClass:NSArray.class] && ![propertyClass isSubclassOfClass:NSSet.class]) {
+                            @throw [NSException exceptionWithName:@"AKPropertyMapException"
+                                                           reason:@"object representation conformsToProtocol NSFastEnumeration, but property class is not subclassOfClass NSArray or NSSet"
+                                                         userInfo:@{ @"objectMap" : objectMap,
+                                                                     @"key" : key,
+                                                                     @"representation" : representation,
+                                                                     @"propertyClass" : propertyClass,
+                                                                     }];
+                            return ;
+                        }
+                        BOOL targetMutable = [propertyClass instancesRespondToSelector:@selector(addObject:)];
+                        NSMutableSet *mutableContainer = [propertyClass new];
+                        if (!targetMutable) mutableContainer = mutableContainer.mutableCopy;
+                        for (NSDictionary *objRepresentation in obj) {
+                            id newPropertyInstance = [mapClass instatiateWithExternalRepresentation:objRepresentation objectMap:propertyMap.objectMap];
+                            if (newPropertyInstance) {
+                                [mutableContainer addObject:newPropertyInstance];
+                            }
+                        }
+                        if (!targetMutable) mutableContainer = mutableContainer.copy;
+                        [self try2setObject:mutableContainer forKey:propertyKey];
+                    } else {
+                        id newPropertyInstance = [mapClass instatiateWithExternalRepresentation:obj objectMap:propertyMap.objectMap];
+                        [self try2setObject:newPropertyInstance forKey:propertyKey];
                     }
-                    [self setValue:mutableArray.copy forKey:propertyKey];
                 } else {
-                    id newPropertyInstance = [propertyMap.objectClass instatiateWithExternalRepresentation:obj objectMap:propertyMap.objectMap];
-                    [self setValue:newPropertyInstance forKey:propertyKey];
+                    @throw [NSException exceptionWithName:@"AKPropertyMapException"
+                                                   reason:@"propertyMap.objectClass not conformsToProtocol <AKObjectMapping>"
+                                                 userInfo:@{ @"objectMap" : objectMap,
+                                                             @"key" : key,
+                                                             @"propertyMap" : propertyMap,
+                                                             @"propertyMap.objectClass" : propertyMap.objectClass }];
                 }
             } else {  //automap
-                objc_property_t property = class_getProperty(object_getClass(self), [propertyKey UTF8String]);
-                if (property) {
-                    Class propertyClass = property_getClass(property);
-                    if (propertyClass && [propertyClass conformsToProtocol:@protocol(AKObjectMapping)]) {
+                if (propertyClass) {
+                    if ([propertyClass conformsToProtocol:@protocol(AKObjectMapping)]) {
                         id newPropertyInstance = [propertyClass instatiateWithExternalRepresentation:obj objectMap:propertyMap.objectMap];
-                        [self setValue:newPropertyInstance forKey:propertyKey];
-                    } else {
-                        [self try2setObject:obj forKey:propertyKey]; // try to set representation object to property as is (JSON object representation)
+                        [self try2setObject:newPropertyInstance forKey:propertyKey];
+                        return ;
                     }
-                } else {
-                    NSLog(@"[WARNING] Representation keyed '%@' skipped: setter not found.", propertyKey);
+                    if ([propertyClass isSubclassOfClass:NSDate.class]) {
+                        NSDateFormatter *df = self.class.dateFormatters[key] ?: self.class.defaultDateFormatter;
+                        if (df) {
+                            [self try2setObject:[df dateFromString:obj] forKey:propertyKey];
+                            return ;
+                        }
+                    } else {
+                        if ([propertyClass isSubclassOfClass:NSURL.class]) {
+                            [self try2setObject:[NSURL URLWithString:obj] forKey:propertyKey];
+                            return ;
+                        }
+                        if ([propertyClass isSubclassOfClass:NSUUID.class]) {
+                            [self try2setObject:[AKUUID UUIDWithUUIDString:obj] forKey:propertyKey];
+                            return ;
+                        }
+                    }
                 }
+                [self try2setObject:obj forKey:propertyKey]; // try to set representation object to property as is (JSON object representation)
             }
+            
         }];
     }
     return self;
 }
 
 Class property_getClass(objc_property_t property) {
+    if (!property) return nil;
     char *type = property_copyAttributeValue(property, "T");
     unsigned long len = strlen(type);
     if (type[0] == '@' && len > 3) {
@@ -116,7 +169,11 @@ static Class __NSCFBooleanClass = nil;
 }
 
 + (NSDateFormatter *)defaultDateFormatter {
-    return nil;
+    NSDateFormatter *df = [NSDateFormatter new];
+    df.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    df.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+    df.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
+    return df;
 }
 
 + (NSString *)stringFromBoolean:(BOOL)b {
